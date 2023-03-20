432d4d43b798240af32eeaf033479bfee7b4c77f
PAARKAVYB <paarkavy.bv@gmail.com>
2023-03-20 19:13:32 +0530

    upload

---
+++ Palindrome/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace PALINDROME
+{
+    public class Palindrome
+    {
+        static void Main(string[] args)
+        {
+            string word, reverse = "";
+            word = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("The string is:" + word);
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reverse += word[i];
+            }
+            if (reverse == word)
+            {
+                Console.WriteLine("The given string {0} is Palindrome", reverse);
+            }
+            else
+            {
+                Console.WriteLine("The given string word is not Palindrome", reverse);
+            }
+            Console.ReadLine();
+        }
+    }
+}
